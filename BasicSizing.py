7c92945aeeb2021e9c37a0d7da95de0f6aff1ab9
Sanchit Arora <tosanchitarora@gmail.com>
2026-02-23 15:05:02 -0800

    nozzle contour works!

--- BasicSizing.py
+++ BasicSizing.py
@@ -7,8 +7,9 @@ import matplotlib.pyplot as plt
 thrust = 1556.87757 #[N], 350lbf 
 Pc = 1723689.3233 #chamber pressure[Pa], 250 psia
 OF = 3 #oxidize-fuel ratio
-CR = 4 #contraction ratio Ac/At
-L_star = 1.0 #characteristic length [m]
+CR = 5 #contraction ratio Ac/At
+L_star = 0.381 #characteristic length [m], 20"
+percent_bell = 0.8 #percent rao nozzle
 
 #GENERAL CALCULATIONS
 c = 1603.9 #effective exhaust velocity [m/s]
@@ -25,6 +26,7 @@ A_t = c_star*m_dot_total/Pc #throat area [m^2]
 d_t = 2 * np.sqrt(A_t/np.pi) #throat diameter [m]
 A_e = A_t * ER #exit area [m^2]
 d_e = 2 * np.sqrt(A_e/np.pi) #throat diameter [m]
+L_n = percent_bell * (np.sqrt(ER)-1)*(d_t/2)/(np.tan(np.deg2rad(15)))
 
 #CHAMBER CALCULATIONS
 A_c = CR*A_t
@@ -32,15 +34,41 @@ d_c = 2*np.sqrt(A_c/np.pi) #chamber diameter [m]
 V_c = L_star * A_t #chamber colume [m^3]
 L_c = V_c/A_c #chamber length (from injector face to throat) [m]
 
+#THETA CALCS
+# 80% Rao Nozzle empirical data
+eratio     = [4,    5,    10,   20,   30,   40,   50,   100]
+theta_n_80 = [21.5, 23.0, 26.3, 28.8, 30.0, 31.0, 31.5, 33.5]
+theta_e_80 = [14.0, 13.0, 11.0, 9.0,  8.5,  8.0,  7.5,  7.0]
+
+# Manual linear extrapolation for ER < 4
+if ER < eratio[0]:
+    # Calculate slope (m) between the first two points
+    m_n = (theta_n_80[1] - theta_n_80[0]) / (eratio[1] - eratio[0])
+    m_e = (theta_e_80[1] - theta_e_80[0]) / (eratio[1] - eratio[0])
+    
+    # y = y1 + m(x - x1)
+    theta_n_deg = theta_n_80[0] + m_n * (ER - eratio[0])
+    theta_e_deg = theta_e_80[0] + m_e * (ER - eratio[0])
+else:
+    theta_n_deg = np.interp(ER, eratio, theta_n_80)
+    theta_e_deg = np.interp(ER, eratio, theta_e_80)
+
+theta_n = (theta_n_deg)
+theta_e = (theta_e_deg)
+
 #PRINT OUTPUTS
 print(f"Total mass flow: {m_dot_total: .3f} kg/s")
 print(f"Fuel mass flow: {m_dot_fuel: .3f} kg/s")
-print(f"Oxidizer mass flow: {m_dot_ox: .3f} kg/s")
+print(f"Oxidizer mass flow: {m_dot_ox: .3f} kg/s\n")
 
 print(f"Chamber diameter: {d_c: .6f} m")
 print(f"Throat diameter: {d_t: .6f} m")
 print(f"Exit diameter: {d_e: .6f} m")
-print(f"Chamber lengthL {L_c: .6f} m")
+print(f"Chamber length: {L_c: .6f} m")
+print(f"Nozzle length; {L_n: .6f} m\n")
+
+print(f"Theta N : {theta_n: .6f} deg")
+print(f"Theta E: {theta_e: .6f} deg\n")
 
 
 
--- NozzleContour.py
+++ NozzleContour.py
@@ -1,135 +1,119 @@
 import numpy as np
-import sympy as sp
 import matplotlib.pyplot as plt
 
-#http://www.aspirespace.org.uk/downloads/Thrust%20optimised%20parabolic%20nozzle.pdf
+# http://www.aspirespace.org.uk/downloads/Thrust%20optimised%20parabolic%20nozzle.pdf
+# https://rrs.org/2023/01/28/making-correct-parabolic-nozzles/
+# https://wikis.mit.edu/confluence/pages/viewpage.action?pageId=153816550
 
 #INPUT PARAMETERS
 ER = 3.4826 #expansion ratio
-CR = 4 #contraction ratio
+CR = 5 #contraction ratio
 r_c = 0.057456/2 #chamber rad.
 r_t = 0.028728/2 #throat rad.
 r_e = np.sqrt(ER)*r_t #exit rad.
-L_star = 1.0 #characteristic length
-L_c = 0.25 #chamber length
+L_c = 0.0762 #chamber length [m]
 
 percent_bell = 0.8 #Percent Rao nozzle. 80% standard
-theta_n = 21 * (np.pi/180) #copied from PSP-Liq CMS due to similar exp. ratios (I'm lazy)
-theta_e = 15 * (np.pi/180) #copied from PSP-Liq CMS due to similar exp. ratios
+theta_n = np.deg2rad(20.7239)
+theta_e = np.deg2rad(14.5174)
 convergence_angle = 37.5
 
 
 #CALCULATIONS
-#Rao converging section
-theta_convergence = np.linspace(-3/4*np.pi, -np.pi/2)
-x_convergence = 1.5 * r_t * np.cos(theta_convergence)
-y_convergence = 1.5 * r_t * np.sin(theta_convergence) + 1.5 * r_t + r_t
+#Area calcs
+A_c = np.pi * (r_c**2) #chamber area
+A_t = np.pi * (r_t**2) #throat area
+A_e = np.pi * (r_e**2) #exit area
 
-A_c = np.pi * (r_c**2)
-A_t = np.pi * (r_t**2)
-A_e = np.pi * (r_e**2)
+#Length calcs
+L_n = percent_bell * (np.sqrt(ER)-1)*r_t/(np.tan(np.deg2rad(15))) #nozzle length eqt. 3
+
+#THROAT AND BELL GEOMETRY
+beta = np.deg2rad(convergence_angle)
+
+#Rao converging section
+# Starts exactly at -90 degrees minus beta (to meet the cone) and ends at -90
+theta_convergence = np.linspace(-np.pi/2 - beta, -np.pi/2, 50)
+x_converging = 1.5 * r_t * np.cos(theta_convergence)
+y_converging = 1.5 * r_t * np.sin(theta_convergence) + 1.5 * r_t + r_t #eqt. 4
 
 #Rao diverging section
 theta_divergence = np.linspace(-np.pi/2, theta_n-(np.pi/2), 200)
 x_divergence = 0.382 * r_t * np.cos(theta_divergence)
-y_divergence = 0.382 * r_t * np.sin(theta_divergence) + 0.382 * r_t + r_t
-
-#Finding E
-x_E = percent_bell * (((np.sqrt(ER)-1)*r_t)/np.tan(np.pi/12))
-y_E = r_e
+y_divergence = 0.382 * r_t * np.sin(theta_divergence) + 0.382 * r_t + r_t #eqt. 5
 
 #Finding N
 x_N = x_divergence[-1]
-y_N = y_divergence[-1]
+y_N = y_divergence[-1] #found by setting angle to (theta_n -90) in eqt. 5
+
+#Finding E
+x_E = L_n #derived from eqt. 3
+y_E = r_e #derived from eqt. 2 or just BasicSizing results
 
 #Finding Q
-m1 = np.tan(theta_n)
-m2 = np.tan(theta_e)
-c1 = y_N - m1*x_N
-c2 = y_E - m2*x_E
-x_Q =(c2 - c1)/(m1 - m2)
-y_Q = (m1*c2 - m2*c1)/(m1 - m2)
+m1 = np.tan(theta_n) #eqt. 8
+m2 = np.tan(theta_e) #eqt. 8
+c1 = y_N - m1*x_N #eqt. 9
+c2 = y_E - m2*x_E #eqt. 9
+x_Q =(c2 - c1)/(m1 - m2) #eqt. 10
+y_Q = (m1*c2 - m2*c1)/(m1 - m2) #eqt. 10
 
 #Rao bell section
 t = np.linspace(0,1,30)
-x_bell = ((1-t)**2) * x_N + 2*(1-t)*t*x_Q + t**2*x_E
-y_bell = ((1-t))**2 * y_N + 2*(1-t)*t*y_Q + t**2*y_E
+x_bell = ((1-t)**2) * x_N + 2*(1-t)*t*x_Q + (t**2)*x_E
+y_bell = ((1-t)**2) * y_N + 2*(1-t)*t*y_Q + (t**2)*y_E
 
-x_p1 = min(x_convergence)
-y_p1 = max(y_convergence)
+#CHAMBER TRANSITION GEOMETRY
+# Define P1 (start of throat entry)
+x_p1 = -1.5 * r_t * np.sin(beta) # The x-coord where the throat arc meets the cone
+y_p1 = 1.5 * r_t * (1 - np.cos(beta)) + r_t
 
-Len_ConvergeCombustor = (r_c - max(y_convergence)) / np.tan(np.deg2rad(convergence_angle))
-x_p2 = x_p1 - Len_ConvergeCombustor
-y_p2 = r_c; #Determined from r_t input and ER
+# Define p2 (End of straight chamber / Start of shoulder)
+# x_p2 is calculated by finding the horizontal distance of the shoulder + the straight cone
+y_sh_end = r_c - 1.5 * r_t * (1 - np.cos(beta))
+x_gap = (y_sh_end - y_p1) / np.tan(beta)
+x_sh_width = 1.5 * r_t * np.sin(beta)
 
-#Solve for intersections at point 3
-f = sp.symbols('f')
+x_p2 = x_p1 - x_gap - x_sh_width
+y_p2 = r_c
 
-line_p1 = sp.tan(-1 * (sp.pi / 4)) * (f - x_p1) + y_p1
-line_p2 = sp.tan(-1*np.deg2rad(convergence_angle)) * (f - x_p2) + y_p2
+# Create the Shoulder Curve (Inwards curve)
+t_sh = np.linspace(0, beta, 30)
+x_sh = x_p2 + 1.5 * r_t * np.sin(t_sh)
+y_sh = r_c - 1.5 * r_t * (1 - np.cos(t_sh))
 
-eqn = sp.Eq(line_p1, line_p2)
-s = sp.solve(eqn, f)[0]
+# Create the Straight Transition (The Cone)
+x_straight = np.linspace(x_sh[-1], x_p1, 20)
+y_straight = np.linspace(y_sh[-1], y_p1, 20)
 
-x_p3 = float(s)
-y_p3 = float(sp.tan(-1*np.deg2rad(convergence_angle)) * (x_p3 - x_p2) + y_p2)
+# Combine the shoulder curve and the straight cone into one transition segment
+x_trans = np.concatenate([x_sh, x_straight])
+y_trans = np.concatenate([y_sh, y_straight])
 
 
-#Converging chamber bezier
-x_ConvergingBezier = ((1 - t)**2 * x_p2 + 2*(1 - t)*t * x_p3 + t**2 * x_p1)
-y_ConvergingBezier = ((1 - t)**2 * y_p2 + 2*(1 - t)*t * y_p3 + t**2 * y_p1)
+#FINAL ARRAY COMBINING
+# Chamber cylinder
+x_chamber = np.array([x_p2 - L_c, x_p2])
+y_chamber = np.array([r_c, r_c])
 
-#Combine partial coords
-x_Partial = np.concatenate([x_ConvergingBezier, x_convergence, x_divergence, x_bell])
-y_Partial = np.concatenate([y_ConvergingBezier, y_convergence, y_divergence, y_bell])
+# Concatenate all parts
+x_full = np.concatenate([x_chamber, x_trans, x_converging, x_divergence, x_bell])
+y_full = np.concatenate([y_chamber, y_trans, y_converging, y_divergence, y_bell])
 
-# Keep only unique x values
-unique_vals, unique_index = np.unique(x_Partial, return_index=True)
-unique_index_sorted = np.sort(unique_index)
+# Clean up duplicates and sort
+_, unique_indices = np.unique(x_full, return_index=True)
+x_plot = x_full[np.sort(unique_indices)]
+y_plot = y_full[np.sort(unique_indices)]
 
-x_Partial = x_Partial[unique_index_sorted]
-y_Partial = y_Partial[unique_index_sorted]
-
-x_fullConverge = np.concatenate([x_ConvergingBezier, x_convergence])
-y_fullConverge = np.concatenate([y_ConvergingBezier, y_convergence])
-
-# Chamber volume calculations
-#V_chamber = L_star * A_t
-#V_converge = abs(np.pi * np.trapezoid(np.flip(x_fullConverge), np.flip(y_fullConverge**2)))
-#V_cylinder = V_chamber - V_converge
-#L_c = V_cylinder / (np.pi * r_c**2)
-
-# Final coordinate matrix
-x_Chamber = np.array([
-    np.min(x_ConvergingBezier) - L_c,
-    np.min(x_ConvergingBezier)
-])
-
-y_Chamber = np.array([r_c, r_c])
-
-x_Total = np.concatenate([x_Chamber, x_ConvergingBezier, x_convergence, x_divergence, x_bell])
-y_Total = np.concatenate([y_Chamber, y_ConvergingBezier, y_convergence, y_divergence, y_bell])
-z_Total = np.zeros(len(x_Total))
-
-# Keep only unique x values
-unique_vals, unique_index = np.unique(x_Total, return_index=True)
-unique_index_sorted = np.sort(unique_index)
-
-Total = np.column_stack([x_Total[unique_index_sorted], y_Total[unique_index_sorted], z_Total[unique_index_sorted]])
-
-x_plot = x_Total[unique_index_sorted]
-y_plot = y_Total[unique_index_sorted]
-
-# Write to file
-np.savetxt("nozzle_contour.txt", Total, delimiter="\t", fmt="%.5f")
-
-# PLOTTING
-plt.figure()
-plt.plot(x_plot, y_plot, color="black")
-plt.plot(x_plot, -1*y_plot, color="black")
-plt.title("Nozzle Contour")
-plt.xlabel("meters")
-plt.ylabel("meters")
+#PLOTTING
+plt.figure(figsize=(10, 4))
+plt.plot(x_plot, y_plot, color="black", linewidth=2)
+plt.plot(x_plot, -y_plot, color="black", linewidth=2)
+plt.fill_between(x_plot, -y_plot, y_plot, color='lightgray', alpha=0.3)
+plt.title(f"Rao Nozzle Profile (ER={ER:.2f})")
+plt.xlabel("Length (m)")
+plt.ylabel("Radius (m)")
+plt.axis("equal")
+plt.grid(True, linestyle='--', alpha=0.6)
 plt.show()
-
 
